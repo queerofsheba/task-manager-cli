1745b77ca6db6b764f1321ce2bbc1623059473d3
Jade Goodwin-Carter <jgoodwincarter@gmail.com>
2026-05-14 10:35:23 -0700

    Add docstrings and improve comments

--- task_manager.py
+++ task_manager.py
@@ -4,27 +4,47 @@ import datetime as dt
 import argparse
 
 def load_tasks():
-    if os.path.exists('tasks.json'):
-        with open('tasks.json', 'r') as tasklist:
+    """
+    Load list of tasks from the JSON file
+
+    Returns:
+        List of task dictionaries, or empty list if file doesn't exist
+    """
+    if os.path.exists("tasks.json"):
+        with open("tasks.json", "r") as tasklist:
             contents = json.load(tasklist)
             return contents
     else:
         return []
 
 def save_tasks(tasks):
-    with open('tasks.json', 'w') as tasklist:
+    """
+    Save tasks to the JSON file
+
+    Arguments:
+        List of task dictionaries to save
+    """
+    with open("tasks.json", "w") as tasklist:
         json.dump(tasks, tasklist, indent=2)
 
 def add_task(tasks, task_name):
+    """
+    Add new task to list of tasks and save to JSON file
+
+    Arguments:
+        List of task dictionaries to add to
+        Name/description of new task
+    """
     # Check for empty string
     if task_name.strip() == "":
-        print(f'Error: Input required.')
+        print("Error: Input required.")
         return
     # Create new task dictionary item
     new_task = {}
     if len(tasks) == 0:
         new_task["id"] = 1
     else:
+        # Generate next available ID
         new_task["id"] = max(task["id"] for task in tasks) + 1
     new_task["name"] = task_name
     new_task["completed"] = False
@@ -37,11 +57,18 @@ def add_task(tasks, task_name):
     save_tasks(tasks)
 
     # Print confirmation message
-    print("New task added!")
+    print(f"New task added - {task_name}")
 
 def view_tasks(tasks):
+    """
+    View formatted list of tasks
+
+    Arguments:
+        List of task dictionaries to display
+    """
+    # Check for empty list
     if len(tasks) == 0:
-        print('No tasks found.')
+        print("Error: No tasks found.")
     else:
         print(f'{"#":4}{"ID":3} | {"Tasks":25}')
         print("-" * 35)
@@ -54,45 +81,62 @@ def view_tasks(tasks):
                 status = "[\u2713]"
             else:
                 status = "[ ]"
-            print(f'{count}.  {id:0>3} | {name:20}{status:^5}')
+            print(f"{count}.  {id:0>3} | {name:20}{status:^5}")
             count += 1
 
 def complete_task(tasks, task_id):
+    """
+    Mark task from list as complete and save to JSON file
+
+    Arguments:
+        List of task dictionaries
+        ID of task to be marked complete
+    """
+    # Check for invalid integer input
     if task_id <= 0:
-        print('Error: Task ID must be positive, non-zero integer.')
+        print("Error: Task ID must be positive, non-zero integer.")
         return
     found = False
     for task in tasks:
-        if task['id'] == task_id:
+        if task["id"] == task_id:
             found = True
-            task['completed'] = True
-            print(f'Task with ID {task_id} marked complete!')
+            task["completed"] = True
+            print(f"Task with ID {task_id} marked complete.")
             save_tasks(tasks)
             return
     if found == False:
-        print(f'Error: Task with ID {task_id} not found. Use \'list\' to see all tasks.')
+        print(f"Error: Task with ID {task_id} not found. Use \'list\' to see all tasks.")
 
 def delete_task(tasks, task_id):
+    """
+    Delete task from list and save to JSON file
+
+    Arguments:
+        List of task dictionaries
+        ID of task to be deleted
+    """
+    # Check for invalid integer input
     if task_id <= 0:
-        print('Error: Task ID must be positive, non-zero integer.')
+        print("Error: Task ID must be positive, non-zero integer.")
         return
+    # Check for empty list
     if len(tasks) == 0:
-        print('Error: No tasks found')
+        print("Error: No tasks found.")
         return
     found = False
     for task in tasks:
-        if task['id'] == task_id:
+        if task["id"] == task_id:
             found = True
             tasks.remove(task)
-            print(f'Task with ID {task_id} removed')
+            print(f"Task with ID {task_id} removed.")
             save_tasks(tasks)
             return
     if found == False:
-        print(f'Error: Task with ID {task_id} not found. Use \'list\' to see all tasks.')
+        print(f"Error: Task with ID {task_id} not found. Use \'list\' to see all tasks.")
 
 def main():
     parser = argparse.ArgumentParser(description="Task Manager CLI")
-    subparsers = parser.add_subparsers(dest='command', help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     # List subcommand
     parser_list = subparsers.add_parser("list", help="View all tasks")
